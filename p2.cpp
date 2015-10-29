36159de44b5187b89d49407a55bd7d19e73d37d8
Krish Munot <krishmunot@gmail.com>
2015-10-30 00:03:07 +0530

    add loops

--- p2.cpp
+++ p2.cpp
@@ -16,6 +16,13 @@ class NumberGameAgain{
 		
 		long long solve(int k, vector<long long> table){
 			vector<bool> isValid(table.size(),true);
+			for(int i=0;i<table.size();i++){
+				for(int j=i+1;j<table.size();j++){
+					if(isReducible(table[i],table[j]))
+						isValid[j]=false;
+					else if(isReducible(table[j],table[i]))
+						isValid[i]=false;
+				}
 		}
 		
 
